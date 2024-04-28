b3c2e2fec964e4eaf2e2e32268a8b71baaf84f79
Sualp Danacı <sualpdnc@gmail.com>
2024-04-28 16:48:13 +0300

    Initial Commit

---
+++ Economate/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Economate
+//
+//  Created by Sualp Danacı on 28.04.2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Economate/EconomateApp.swift
@@ -0,0 +1,17 @@
+//
+//  EconomateApp.swift
+//  Economate
+//
+//  Created by Sualp Danacı on 28.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct EconomateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
